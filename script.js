a7d87c91eff810b114dff3e4eafc1df7b8ca6bdd
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-02 12:10:52 +0330

    js code...

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let  btn=document.querySelector('a');
 btn.addEventListener('click',function(event){
 
+    let x=event.clientX;
+    let y=event.clientY;
+
+    let tag=document.createElement('span');
+    tag.style.left=x+'px';
+    tag.style.top=y+'px';
+    this.appendChild(tag);
 
 
-    
+
 })
